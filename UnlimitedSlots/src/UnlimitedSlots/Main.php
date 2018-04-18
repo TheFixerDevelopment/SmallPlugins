ea1875ec13d2a78ad477a5c7ce7ac5cf1d3f7465
TheFixerDevelopment <30442910+TheFixerDevelopment@users.noreply.github.com>
2018-04-18 13:29:36 +0100

    Update Main.php

--- UnlimitedSlots/src/UnlimitedSlots/Main.php
+++ UnlimitedSlots/src/UnlimitedSlots/Main.php
@@ -13,7 +13,7 @@ class Main extends PluginBase implements Listener {
     }
 
     public function onQuery(QueryRegenerateEvent $event) {
-        $count = $event->setPlayerCount(intval(count));
+        $event->setPlayerCount(intval(count));
         $event->setMaxPlayerCount(intval(count($this->getServer()->getOnlinePlayers())+$count));
     }
 }
